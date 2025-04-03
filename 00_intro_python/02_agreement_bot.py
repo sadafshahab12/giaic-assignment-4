8a6c4adf5c8058c2094462dc2b4db33f1cdda26c
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-04-03 14:30:33 +0500

    continue

---
+++ 00_intro_python/02_agreement_bot.py
@@ -0,0 +1,19 @@
+# Write a program which asks the user what their favorite animal is, and then always responds with "My favorite animal is also ___!" (the blank should be filled in with the user-inputted animal, of course).
+
+# Here's a sample run of the program (user input is in bold italics - note the space between the prompt and the user input!):
+
+# What's your favorite animal? cow
+
+# My favorite animal is also cow!
+# user_favourtie_animal = input("What is your favourite animal?")
+# print(f"My favourite animal is also {user_favourtie_animal}!")
+
+
+def main():
+    user_input = input("What is your favourite animal?")
+
+    print(f"\n\nMy favourite animal is also \033[1m\033[3m{user_input}!\033[0m")
+
+
+if __name__ == "__main__":
+    main()
---
+++ 00_intro_python/04_how_old_are_they.py
@@ -0,0 +1,27 @@
+# Write a program to solve this age-related riddle!
+
+# Anton, Beth, Chen, Drew, and Ethan are all friends. Their ages are as follows:
+# Anton is 21 years old.
+# Beth is 6 years older than Anton.
+# Chen is 20 years older than Beth.
+# Drew is as old as Chen's age plus Anton's age.
+# Ethan is the same age as Chen.
+
+# Your code should store each person's age to a variable and print their names and ages at the end. The autograder is sensitive to capitalization and punctuation, be careful! Your solution should look like this (the below numbers are made up -- your solution should have the correct values!):
+
+
+def main():
+    anton = 21
+    beth = anton + 6
+    chen = beth + 20
+    drew = anton + chen
+    ethan = chen
+
+    print(f"Anton is {anton}")
+    print(f"Beth is {beth}")
+    print(f"Chen is {chen}")
+    print(f"Drew is {drew}")
+    print(f"Ethan is {ethan}")
+
+if __name__ == "__main__":
+    main()
