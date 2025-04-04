85b035a86fa07d4c927bc1acd030d41d2dfd113c
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-04-04 15:45:16 +0500

    01_ exp_ 04_pythagorean_theorem done

--- 01_expressions/03_feet_to_inches.py
+++ 01_expressions/03_feet_to_inches.py
@@ -1,1 +1,12 @@
-Converts feet to inches. Feet is an American unit of measurement. There are 12 inches per foot. Foot is the singular, and feet is the plural.
+# Converts feet to inches. Feet is an American unit of measurement. There are 12 inches per foot. Foot is the singular, and feet is the plural.
+
+per_foot_inches = 12
+def main():
+  feet = float(input("Enter feet: "))
+  inches = feet * per_foot_inches
+  print(f"{feet} feet is {inches} inches")
+
+if __name__ == "__main__":
+  main()
+  
+  
