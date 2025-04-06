6c5e1ac8c038cd4be304f3a3020b992f33db856c
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-04-06 21:51:25 +0500

    done

---
+++ 02_lists/08_shorten.py
@@ -0,0 +1,28 @@
+# Fill out the function shorten(lst) which removes elements from the end of lst, which is a list, and prints each item it removes until lst is MAX_LENGTH items long. If lst is already shorter than MAX_LENGTH you should leave it unchanged. We've written a main() function for you which gets a list and passes it into your function once you run the program. For the autograder to pass you will need MAX_LENGTH to be 3, but feel free to change it around to test your program.
+
+MAX_LENGTH = 3
+
+def shorten(lst):
+    while len(lst) > MAX_LENGTH:
+        remove_item = lst.pop()
+        print(remove_item)
+
+
+def get_list():
+    """prompt the user to enter one element of the list at a time and returns the returning list"""
+    items = []
+    elem = input("Please enter an element of the list or press enter to stop.")
+    while elem != "":
+        items.append(elem)
+        elem = input("Please enter an element of the list or press enter to stop.")
+    return items
+
+
+def main():
+    elem_list = get_list()
+    shorten(elem_list)
+    print(elem_list)
+
+
+if __name__ == "__main__":
+    main()
