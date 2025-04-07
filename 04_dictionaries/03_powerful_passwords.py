b4786e36db8730f8b7de746dabc6214943f95a48
Sadaf Shahab <sadafshahab123@yahoo.com>
2025-04-07 16:08:42 +0500

    done

--- 04_dictionaries/03_powerful_passwords.py
+++ 04_dictionaries/03_powerful_passwords.py
@@ -11,3 +11,40 @@
 # Fill out the login(...) function for a website that hashes their passwords. Login should return True if an email's stored password hash in stored_logins is the same as the hash of password_to_check.
 
 # (Hint. You will need to use the provided hash_password(...) function. You don't necessarily need to know how it works, just know that hash_password(...) returns the hash for the password!)
+
+from hashlib import sha256
+
+
+def login(email, stored_logins, password_to_check):
+    if stored_logins[email] == hash_password(password_to_check):
+        return True
+    return False
+
+
+def hash_password(password):
+    """take in password and return hash value
+    Input : The Password we want
+    Output : The Hash of the password
+    """
+    return sha256(password.encode()).hexdigest()
+
+
+def main():
+    stored_logins = {
+        "example@gmail.com": "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",
+        "code_in_placer@cip.org": "973607a4ae7b4cf7d96a100b0fb07e8519cc4f70441d41214a9f811577bb06cc",
+        "student@stanford.edu": "882c6df720fd99f5eebb1581a1cf975625cea8a160283011c0b9512bb56c95fb",
+    }
+
+    print(login("example@gmail.com", stored_logins, "password123"))
+    print(login("example@gmail.com", stored_logins, "password123"))
+
+    print(login("code_in_placer@cip.org", stored_logins, "Karel"))
+    print(login("code_in_placer@cip.org", stored_logins, "Karel"))
+
+    print(login("student@stanford.edu", stored_logins, "password"))
+    print(login("student@stanford.edu", stored_logins, "123!456?789"))
+
+
+if __name__ == "__main__":
+    main()
